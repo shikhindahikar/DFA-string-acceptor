0840d72ff1159e1194419585e61f99fa485a3eb0
Shikhin Dahikar <31371540+shikhindahikar@users.noreply.github.com>
2019-09-09 18:16:17 +0530

    Add files via upload

--- TOC1.cpp
+++ TOC1.cpp
@@ -4,42 +4,79 @@
 
 using namespace std;
 
-vector<char> input(){
-    string in;
-    vector<char> vect_in;
-    cout<<"Enter the list of symbols"<<endl;
-    getline(cin, in);
-    int i=0;
-    while(i!=in.end()){
-        if(in[i]!=" "){
-            vect_in.push_back(in[i]);
-        }
-        i++;
-    }
-    return vect_in;
-}
-
-vector<vector<string>> input_matrix(int n){
+vector< vector<string> > input_matrix(int n){
     int i, j, k;
     string x;
-    vector<vector<string>> moves;
+    vector< vector<string> > moves;
     for(i=0;i<n;i++){
-        moves[i].push_back(getline(cin, x));
+            cout<<"For state "<<i<<endl;
+        for(j=0;j<n;j++){
+            cout<<"For state "<<j<<endl;
+            cin>>x;
+            moves[i].push_back(x);
+
+
+        }
     }
     return moves;
 }
 
-int accept(vector<char> chars, string query, int n, vector<vector<string>> moves){
-    int i, j, state=0;
-    for(i=0;i<query.end();i++){
+bool hasA(char x, string str){
+    int i=0;
+    bool res=false;
+    while(str[i]!='\0'){
+        if(x==str[i]){
+            res=true;
+        }
+    }
+    return res;
+}
+
+int accept(string chars, vector< vector<string> > moves, string query){
+    int i=0, k, state=0,  flag;
+    while(query[i]!='\0'){
+        k=0; flag=0;
+        while(k<moves.size() && flag==0){
+                if(hasA(query[i], moves[state][k])){
+                    state=k;flag=1;
+            }
+            k++;
+        }
+        i++;
+    }
+    if(state==moves.size()-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int main(){
+    string symbols;
+    cout<<"Enter list of symbols as a string\n";
+    cin>>symbols;
+    vector< vector<string> > mat=input_matrix(symbols.size());
+    cout<<"Enter any string to check\n";
+    string query;
+    cin>>query;
+    if(accept(symbols, mat, query)==1){
+        cout<<"accepted\n";
+    }
+    else{
+        cout<<"not accepted\n";
+    }
+    return 0;
+}
+  /* for(i=0;i<query.end();i++){
         for(j=0;j<n;j++){
-            if(query[i]==moves[start][j]){
-                start=j;
+            if(query[i]==moves[state][j]){
+                state=j;
                 break;
             }
         }
-    }
-}
+    } */
+
 
 
 
